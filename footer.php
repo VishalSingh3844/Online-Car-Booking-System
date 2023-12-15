d26ff003c8fc7c6cfceed6e6b0e6138ba0fa07e8
Vishal  Singh <111409536+VishalSingh3844@users.noreply.github.com>
2023-12-15 06:29:22 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer class="py-5 bg-dark">
+    <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; <?php echo date('Y');?> Online Car Booking System | Developer By <i class="fa fa-heartbeat"></i> <a href="">Vishal Singh</a>
+        </p>
+    </div>
+    <!-- /.container -->
+</footer>
